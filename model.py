e224786fe125043cfe08e19bf30e82962d76f752
Benny Elashkin <pavel.elashkin@gmail.com>
2023-03-18 04:04:56 +0300

    Sem_8 def find solved!

--- model.py
+++ model.py
@@ -25,3 +25,19 @@ def add_rec():
             case _:
                 controller.message()
                 return
+
+def find():
+    f = input("Введите элемент поиска: ")
+    lines = read_phonebook()
+    count = 0
+    flag=False
+    
+    for line in lines:
+        if f in line:
+            print(line)
+            count += 1
+            flag = True
+    if flag:
+        print(f'Найдено {count} контактов. Поиск завершен!')
+    else:
+        print('Контакт не найден')
